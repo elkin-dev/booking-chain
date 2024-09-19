842c31fab48c54007182558d300d85fb389d2109
Elkin Imbachi <19elkin.dev@gmail.com>
2024-09-18 22:46:59 -0500

    implements authentication and authorization with keycloak part 1

---
+++ api-gateway/src/main/java/com/elkindev/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.elkindev.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.authorizeExchange(auth -> auth
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return httpSecurity.build();
+    }
+}
